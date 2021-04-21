cd21f19806a66bc9c16d6c149c268f295893aebc
vishwaksenahs <hsvishwak@gmail.com>
2021-04-21 18:07:17 +0530

    Reverse a string

---
+++ Strings/StringOperations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = new char[] { 'h','e','l','l','o' };
+            ReverseString(s);
+        }
+        public static void ReverseString(char[] s)
+        {
+            var j = s.Length - 1;
+            for (i = 0; i < s.Length / 2; i++)
+            {
+                var t = s[i];
+                s[i] = s[j];
+                s[j] = t;
+                j--;
+            }
+        }
+    }
+}
