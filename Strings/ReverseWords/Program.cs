8414223e0c7546920976be1a07ab42e849e6e895
vishwaksenahs <hsvishwak@gmail.com>
2021-04-22 09:56:44 +0530

    Reverse Words of string

---
+++ Strings/ReverseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReverseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "i love programming very much";
+            Console.Write(str);
+            var strArray = str.Split(' ');
+            var outStr = "";
+            for (var i = strArray.Length - 1; i >= 0; i--)
+            {
+                outStr += strArray[i] + ' ';
+            }
+            Console.Write(outStr);
+        }
+    }
+}
