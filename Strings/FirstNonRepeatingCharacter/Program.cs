2f4910df11f6f9da6dba56da5ae9df462d502941
vishwaksenahs <hsvishwak@gmail.com>
2021-04-29 18:17:10 +0530

    Sort String using frequency

--- Strings/FirstNonRepeatingCharacter/Program.cs
+++ Strings/FirstNonRepeatingCharacter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace FirstNonRepeatingCharacter
 {
@@ -8,9 +9,11 @@ namespace FirstNonRepeatingCharacter
     {
         static void Main(string[] args)
         {
-            var s = "dddccdbba";
+            var s = "abababababababababababbbbbbbbbbbbbbbbbbbbbbbb";
             var res = FirstUniqChar(s);
+            var r = FrequencySort(s);
             Console.WriteLine(res);
+            Console.WriteLine(r);
         }
         public static int FirstUniqChar(string s)
         {
@@ -32,6 +35,41 @@ namespace FirstNonRepeatingCharacter
                     return i;
             }
             return -1;
+        }
+        public static string FrequencySort(string s)
+        {
+            int[] tally = new int[126];
+            StringBuilder res = new StringBuilder();
+            Dictionary<char, int> tallys = new Dictionary<char, int>();
+            for (var i = 0; i < s.Length; i++)
+            {
+                tally[s[i]]++;
+                if (tallys.ContainsKey(s[i]))
+                {
+                    tallys[s[i]]++;
+                }
+                else
+                {
+                    tallys[s[i]] = 1;
+                }
+            }
+            var values = tallys.Values.ToArray();
+            values=values.OrderByDescending(x => x).ToArray();
+            for (var i = 0; i < values.Length; i++)
+            {
+                for(var j = 48; j < 123; j++)
+                {
+                    if (values[i] == tally[j])
+                    {
+                        while (tally[j]!=0)
+                        {
+                            res.Append((char)j);
+                            tally[j]--;
+                        }
+                    }
+                }
+            }
+            return res.ToString(); 
         }
     }
 }
