d0f9e762d4d0ca9ac1bd32c547568391090c079c
vishwaksenahs <hsvishwak@gmail.com>
2021-06-10 11:41:06 +0530

    Longest Prefix

---
+++ Strings/LongestCommonPrefix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestCommonPrefix
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs= new string[]{ "flower", "floy", "flow" };
+            Console.WriteLine(LongestCommonPrefix(strs));
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var res = "";
+            if (strs.Length < 1) return "";
+            res = strs[0];
+            for(var i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(res) != 0)
+                {
+                    res = res.Substring(0, res.Length - 1);
+                    if (res=="") return "";
+                }
+            }
+            return res;
+        }
+    }
+}
